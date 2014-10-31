0f4d6ae3e73110e647bcb131c56d348b065bfcaf
cbdweb <nik@cbdweb.net>
2014-10-31 14:50:18 +1100

    include parent CSS;

--- functions.php
+++ functions.php
@@ -1,4 +1,12 @@
 <?php
+
+add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
+function enqueue_child_theme_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('parent-style')  );
+}
+
+
 /*
  * Woo commerce customisation to show link to online retailer for each item
  * http://www.remicorson.com/mastering-woocommerce-products-custom-fields/
